1eedf273f1fcfbbdc61990aef40c4ff731072ca6
Chadwick Green <chadggreen@gmail.com>
2021-05-15 23:26:04 -0400

    project tasks

---
+++ src/Components/projecttasks.js
@@ -0,0 +1,10 @@
+// --------------Project Tasks
+// Create a Landing Page
+// Create a Feed
+// Create a User Profile
+// Create a Politician Profile
+// Create an Explore Page
+// Implement Post Creation
+// Make a Chat interface
+// Implement a Search Bar
+// Set up the back-end server
